0308869cb20cfb2bb27eb86fffa917a2a9c1f0a2
DESKTOP-12N1QU1\DELL <thethtoohtoosan2172000@gmail.com>
2022-10-11 09:21:17 +0630

    doc

--- doc_test/connect_doc_list.php
+++ doc_test/connect_doc_list.php
@@ -1,30 +1,29 @@
 <?php
-//initial connection
- $hostname="location";
- $port=3306;
- $dbname="breathmore";
-$username="root";
-$password="";
+$hostname = "localhost";
+$port = 3306;
+$dbname = "breathmore";
+$username = "root";
+$password = "";
 
-//initial connection
 $pdo = new PDO(
-    "mysql:host=$hostname;port=$port;dbname=$dbname", 
-    $username, $password
+    "mysql:host=$hostname;port=$port;dbname=$dbname",
+    $username,
+    $password
 );
 
-
-//error exception
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 // place to write query
-$sql=$pdo->prepare("SELECT * FROM doctor_lists");
+$sql = $pdo->prepare("SELECT * FROM doctor_lists ");
 
 //execute
 $sql->execute();
 
-$resultList= $sql->fetchAll(PDO::FETCH_ASSOC);
-print_r($resultList);
+$result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+print_r($result);
 
 
 
