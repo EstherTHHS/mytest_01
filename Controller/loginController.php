b30d78cacaa8a88ce559ade71727348ecef998dc
DESKTOP-12N1QU1\DELL <thethtoohtoosan2172000@gmail.com>
2022-10-11 10:25:04 +0630

    no message

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -3,11 +3,24 @@
 
 session_start();
 
+include "../Model/dbConnection.php";
+
 
 if(isset($_POST["username"]) && isset($_POST["pwd"])){
         $name=$_POST["username"];
         $pwd=$_POST["pwd"];
-        echo "OKKKK";
+
+        
+     
+        $sql = $pdo->prepare(
+            "SELECT * FROM admin_management 
+        WHERE admin_name='$name' AND password='$pwd' ");
+
+     //execute
+        $sql->execute();
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        echo "<pre>";
+        print_r($result);
        
-    }
- ?>
+}
