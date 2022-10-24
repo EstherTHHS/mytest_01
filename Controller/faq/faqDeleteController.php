7b82e8830762d8f4fb442eb284937a278265a685
DESKTOP-12N1QU1\DELL <thethtoohtoosan2172000@gmail.com>
2022-10-24 17:32:55 +0630

    contact faq

--- Controller/faq/faqDeleteController.php
+++ Controller/faq/faqDeleteController.php
@@ -8,11 +8,11 @@ if (isset($_GET["id"])) {
     echo $labid;
 
     $sql = $pdo->prepare("
-    UPDATE lab_reports SET del_flg=1
+    UPDATE faq SET del_flg=1
     WHERE id=:id
     ");
     $sql->bindValue(":id", $labid);
     $sql->execute();
 
-    header("Location: ../../View/labreport/labList.php");
+    header("Location: ../../View/faq/faqAdd.php");
 }
